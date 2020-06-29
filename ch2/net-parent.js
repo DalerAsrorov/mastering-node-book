fbc1490516795c35c9f6e8fbd98988b2f1078d85
DalerAsrorov <asrorids@gmail.com>
2020-06-29 01:28:21 -0400

    handling connection in parent/child processes

---
+++ ch2/net-parent.js
@@ -0,0 +1,11 @@
+const path = require("path");
+const child = require("child_process").fork(path.join(__dirname, "net-child.js"));
+const server = require("net").createServer();
+
+server.on("connection", (socket) => {
+  socket.end("Parent handled the connection");
+});
+
+server.listen(8080, () => {
+  child.send("Parent passing down server", server);
+});
